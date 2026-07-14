230b5f09cc640f10232dede1354075b679375786
tubabashir@gmail.com <tubabashir@gmail.com>
2026-07-14 19:33:42 +0530

    powercalcultor

---
+++ Activities/Activity2/powercalculator.py
@@ -0,0 +1,8 @@
+base = 2
+exponent = 3
+
+# Calculate base raised to the power of exponent
+result = base ** exponent
+
+print(f"{base} to the power of {exponent} is {result}")
+# Output: 2 to the power of 3 is 8
