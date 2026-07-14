5f61b477ed20b6d5ccbd910d928d5aad3ded91e3
tubabashir@gmail.com <tubabashir@gmail.com>
2026-07-14 19:43:38 +0530

    reverseorder.py

---
+++ Activities/Activity2/reverseorder.py
@@ -0,0 +1,9 @@
+# Reversing a String
+text = "Python"
+reversed_text = text[::-1]
+print(reversed_text)  # Output: nohtyP
+
+# Reversing a List
+items = ["apple", "banana", "cherry"]
+reversed_items = items[::-1]
+print(reversed_items)  # Output: ['cherry', 'banana', 'apple']
