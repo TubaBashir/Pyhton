43287496bdda8d78d00603883675d6ea3c83caac
tubabashir@gmail.com <tubabashir@gmail.com>
2026-07-14 18:00:03 +0530

    reverse

---
+++ Activities/Activity2/reversestring.py
@@ -0,0 +1,7 @@
+# Input string from user
+text = input("Enter a string: ")
+
+# Reverse using string slicing [start:stop:step]
+reversed_text = text[::-1]
+
+print(f"Reversed string: {reversed_text}")
