1f4fcac05acecb3e883144b0cb0b15309233c943
tubabashir@gmail.com <tubabashir@gmail.com>
2026-07-14 15:44:01 +0530

    bday

---
+++ Activity1/birthdays.py
@@ -0,0 +1,12 @@
+b1="21/09/2000"
+b2="21/09/2009"
+b3="21/09/2010"
+b4="21/09/2011"
+b5="21/09/2000"
+
+print("Birthdays of the people are:")
+print(b1)
+print(b2)
+print(b3)
+print(b4)
+print(b5)   
