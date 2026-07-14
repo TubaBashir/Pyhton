b542d7bfeb6a75988434f8f26fd601d71d8c3301
tubabashir@gmail.com <tubabashir@gmail.com>
2026-07-14 17:16:07 +0530

    condi

---
+++ Activity1/conditional.py
@@ -0,0 +1,8 @@
+age = 20
+
+if age >= 18:
+    print("You are eligible to drive.")
+elif age == 17:
+    print("You can apply for a learner's permit.")
+else:
+    print("You are too young to drive.")
