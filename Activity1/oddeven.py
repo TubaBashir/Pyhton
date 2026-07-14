cd467b1e3ae25e634ec230ddd76d5c9c96f36973
tubabashir@gmail.com <tubabashir@gmail.com>
2026-07-14 17:19:01 +0530

    oddevn

---
+++ Activity1/oddeven.py
@@ -0,0 +1,8 @@
+# Input a number
+num = int(input("Enter an integer: "))
+
+# Check divisibility by 2
+if num % 2 == 0:
+    print(f"{num} is an Even number")
+else:
+    print(f"{num} is an Odd number")
