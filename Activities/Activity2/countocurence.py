8a528ef54ca9b67a79b8599bd77725fedf4d7029
tubabashir@gmail.com <tubabashir@gmail.com>
2026-07-14 19:46:53 +0530

    count

---
+++ Activities/Activity2/countocurence.py
@@ -0,0 +1,7 @@
+text = "banana"
+char_to_count = "a"
+
+count = text.count(char_to_count)
+
+print(f"The character '{char_to_count}' appears {count} times.")
+# Output: The character 'a' appears 3 times.
