b424d424f593898346d45c15cbbe06641893352d
tubabashir@gmail.com <tubabashir@gmail.com>
2026-07-20 16:45:41 +0530

    mathem

---
+++ Activities/Activity2/Mathematicalop.py
@@ -0,0 +1,11 @@
+# Setup variables
+a = 15
+b = 4
+
+print("➕ Addition       (a + b)  :", a + b)   # Output: 19
+print("➖ Subtraction    (a - b)  :", a - b)   # Output: 11
+print("✖️ Multiplication (a * b)  :", a * b)   # Output: 60
+print("➗ Float Division (a / b)  :", a / b)   # Output: 3.75 (Returns a float)
+print("🔲 Floor Division (a // b) :", a // b)  # Output: 3    (Rounds down to nearest integer)
+print("模 Modulo         (a % b)  :", a % b)   # Output: 3    (Returns the remainder)
+print("🚀 Exponent       (a ** b) :", a ** b)  # Output: 50625 (15 to the power of 4)
